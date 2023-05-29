ad12652b1bbb8ae48c0116dc94bf788cdfc8e2b8
qiushaocloud <qiushaocloud@gmail.com>
2023-05-29 15:52:57 +0800

    update code

--- init-k8s.sh
+++ init-k8s.sh
@@ -34,18 +34,22 @@ else
     sed -i "/<IP_AUTODETECTION_METHOD_VALUE>/d" calico.yaml
 fi
 
+echo "start image download"
+bash image_download.sh
+
+echo "start kubeadm init"
 if [ "$controlPlaneEndpoint" != "" ]; then
     # sed -i "s/<CONTROL_PLANE_ENDPOINT>/$controlPlaneEndpoint/g" kubeadm-config.yml
 
     if [ "$k8sToken" != "" ]; then
         # sed -i "s/<K8S_TOKEN>/$k8sToken/g" kubeadm-config.yml
         # sed -i "s/ttl: 24h0m0s/ttl: '0'/g" kubeadm-config.yml
-        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --upload-certs --control-plane-endpoint=$controlPlaneEndpoint --token=$k8sToken --token-ttl=0 --cri-socket unix:///var/run/docker.sock
+        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --upload-certs --control-plane-endpoint=$controlPlaneEndpoint --token=$k8sToken --token-ttl=0 --cri-socket unix:///var/run/docker.sock | tee kubeadm-init.log
     else
         # sed -i "/指定token/d" kubeadm-config.yml
         # sed -i "/<K8S_TOKEN>/d" kubeadm-config.yml
         # sed -i "/ttl: 24h0m0s/d" kubeadm-config.yml
-        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --upload-certs --control-plane-endpoint=$controlPlaneEndpoint --cri-socket unix:///var/run/docker.sock
+        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --upload-certs --control-plane-endpoint=$controlPlaneEndpoint --cri-socket unix:///var/run/docker.sock | tee kubeadm-init.log
     fi
 else
     sed -i "/<CONTROL_PLANE_ENDPOINT>/d" kubeadm-config.yml
@@ -53,20 +57,16 @@ else
     if [ "$k8sToken" != "" ]; then
         # sed -i "s/<K8S_TOKEN>/$k8sToken/g" kubeadm-config.yml
         # sed -i "s/ttl: 24h0m0s/ttl: '0'/g" kubeadm-config.yml
-        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --upload-certs --token=$k8sToken --token-ttl=0 --cri-socket unix:///var/run/docker.sock
+        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --upload-certs --token=$k8sToken --token-ttl=0 --cri-socket unix:///var/run/docker.sock | tee kubeadm-init.log
     else
         # sed -i "/指定token/d" kubeadm-config.yml
         # sed -i "/<K8S_TOKEN>/d" kubeadm-config.yml
         # sed -i "/ttl: 24h0m0s/d" kubeadm-config.yml
-        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --upload-certs --cri-socket unix:///var/run/docker.sock
+        kubeadm init --kubernetes-version v1.21.5 --image-repository registry.aliyuncs.com/google_containers --pod-network-cidr=10.244.0.0/16 --service-cidr=10.1.0.0/16 --apiserver-advertise-address=$apiserverAdvertiseAddress --apiserver-bind-port=$apiserverBindPort --upload-certs --cri-socket unix:///var/run/docker.sock | tee kubeadm-init.log
     fi
 fi
 
-echo "start image download"
-bash image_download.sh
-
-echo "start kubeadm init"
-kubeadm init --config=$PWD/kubeadm-config.yml --upload-certs | tee kubeadm-init.log
+# kubeadm init --config=$PWD/kubeadm-config.yml --upload-certs | tee kubeadm-init.log
 
 CHECK_INIT_OK_STR=`grep "kubeadm join " kubeadm-init.log`
 if [ "$CHECK_INIT_OK_STR" != "" ]; then
